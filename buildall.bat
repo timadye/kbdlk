961448c4ac407f0a524ebac21f85c7d947457e8f
Tim Adye <T.J.Adye@rl.ac.uk>
2019-12-01 02:54:07 +0000

    build*.bat now uses kbdutool to build .dll. These scripts accept more options. kbdlk.klc moved to src.

--- buildall.bat
+++ buildall.bat
@@ -1,29 +1,41 @@
 @ECHO OFF
 SETLOCAL
-IF NOT "%1"=="" GOTO BUILD
-echo "Usage: BUILDALL.BAT NAME [debug | keep]"
-GOTO END
 
-:BUILD
-IF NOT EXIST build MKDIR built
+IF "%1"=="-h" (
+   ECHO Usage: BUILDALL.BAT NAME [debug] [verbose] [keep] [usecl]
+   GOTO END
+)
+IF "%1"=="" (
+   SET NAME=kbdlk
+) ELSE (
+   SET NAME=%1
+)
+
+IF NOT EXIST built MKDIR built
 CHDIR built
-echo Build i386
+
+ECHO Build i386
 IF NOT EXIST i386 MKDIR i386
 CHDIR i386
-call %~dp0buildarch.bat i386 ..\..\src\%1 %2
-echo Build ia64
+CALL %~dp0buildarch.bat i386 ..\..\src\%NAME% %2 %3 %4 %5 %6 %7 %8 %9
+
+ECHO Build ia64
 IF NOT EXIST ..\ia64 MKDIR ..\ia64
 CHDIR ..\ia64
-call %~dp0buildarch.bat ia64 ..\..\src\%1 %2
-echo Build amd64
+CALL %~dp0buildarch.bat ia64 ..\..\src\%NAME% %2 %3 %4 %5 %6 %7 %8 %9
+
+ECHO Build amd64
 IF NOT EXIST ..\amd64 MKDIR ..\amd64
 CHDIR ..\amd64
-call %~dp0buildarch.bat amd64 ..\..\src\%1 %2
-echo Build wow64
+CALL %~dp0buildarch.bat amd64 ..\..\src\%NAME% %2 %3 %4 %5 %6 %7 %8 %9
+
+ECHO Build wow64
 IF NOT EXIST ..\wow64 MKDIR ..\wow64
 CHDIR ..\wow64
-call %~dp0buildarch.bat wow64 ..\..\src\%1 %2
+CALL %~dp0buildarch.bat wow64 ..\..\src\%NAME% %2 %3 %4 %5 %6 %7 %8 %9
+
 CHDIR ..\..\setup
-XCOPY * ..\built\ /Y
+XCOPY * ..\built\ /Y /Q | FIND /V " File(s) copied"
+
 :END
 ENDLOCAL
